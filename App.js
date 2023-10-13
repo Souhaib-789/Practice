2502f33d9c96e9eb5e1e76e2303b1a8ea641e365
Souhaib-789 <souhaibsouhaib838@gmail.com>
2023-10-13 20:00:23 +0500

    check auth

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import AuthStack from "./src/navigations/AuthStack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from '@react-navigation/native';
 import store from './src/redux/Store'
-import { Provider, useSelector } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import AppStack from "./src/navigations/AppStack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { login } from "./src/redux/Actions/AuthAction";
 
 
 
@@ -14,10 +16,22 @@ const App = () => {
 
   const [visible, setvisible] = useState(true)
   const loginState = useSelector(state => state.AuthReducer.isLogin)
+const dispatch = useDispatch()
 
-  // console.log('====================================');
-  // console.log('loginState', loginState);
-  // console.log('====================================');
+  const checkAuthentication = async () => {
+    setTimeout(async () => {
+      let user_data = await AsyncStorage.getItem('@user')
+      if (user_data != null) {
+        dispatch(login(true))
+        const userdata = JSON.parse(user_data)
+        // console.log('====================>>>>>>>>>>>', userdata);
+        dispatch(userInfo(userdata))
+      } else {
+        dispatch(login(false))
+      }
+    }, 1000);
+  }
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
